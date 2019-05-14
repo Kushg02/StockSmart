e836318d7abce5d91d4c8a26027755ecf70fe851
Kush Garg <kushg02@gmail.com>
2019-05-13 20:27:27 -0700

    tried to fix File Reader HashMap key to import values correctly. Did not finish bc want to discuss first.

--- FileIO.java
+++ FileIO.java
@@ -11,18 +11,29 @@ import java.util.*;
 public class FileIO
 {
     public static void main(String[] args) throws FileNotFoundException {
+        
+        
         System.out.println("Enter the file directory: ");
         Scanner in = new Scanner(System.in);
         String file = in.next();
         Scanner input = new Scanner(new File(file));
-        Hashmap<Integer, Double> data = new ;
-        while(input.hasNextLine()) {
+        HashMap<Integer, Double> data = new HashMap<Integer, Double>();
+        while(input.hasNextLine()){
             String line = input.nextLine();
             String[] words = line.split(", ");
-            data.add(Double.parseDouble(words[1]));
+            data.put(Integer.parseInt(words[0]) ,Double.parseDouble(words[1]));
+            System.out.println(Arrays.toString(words));
         }
+        
+      /* Display content using Iterator*/
+      Set set = data.entrySet();
+      Iterator iterator = set.iterator();
+        
         ArrayList<Double> est = new ArrayList<>();
-        for(int i = 0; i < data.size()-1; i++) {
+        System.out.println("data size:" + data.size());
+        System.out.println("data value 0:" + data.get(0));
+
+        for(int i = 0; i < data.size()-2; i++) {
             est.add((data.get(i)+data.get(i+1))/2);
         }
         double slope = est.get(est.size()-1) - est.get(est.size()-2);
