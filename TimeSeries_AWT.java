2563c12e9f2d1e23f6f57b5eff173526113d7688
zhixinlucy <50589128+zhixinlucy@users.noreply.github.com>
2019-05-21 00:38:44 -0700

    Update TimeSeries_AWT.java

--- TimeSeries_AWT.java
+++ TimeSeries_AWT.java
@@ -2,30 +2,29 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel; 
 import org.jfree.chart.JFreeChart; 
 import org.jfree.data.general.SeriesException; 
-import org.jfree.data.time.Second; 
-import org.jfree.data.time.TimeSeries; 
-import org.jfree.data.time.TimeSeriesCollection; 
+import org.jfree.data.time.TimePeriodValues; 
+import org.jfree.data.time.TimePeriodValuesCollection; 
 import org.jfree.data.xy.XYDataset; 
 import org.jfree.ui.ApplicationFrame; 
 import org.jfree.ui.RefineryUtilities;
 import org.jfree.data.time.Day;
 
-public class TimeSeries_AWT extends ApplicationFrame {
+public class Graph extends ApplicationFrame {
 
-   public TimeSeries_AWT( final String title ) {
-      super( title );         
+   public Graph (final String title) {
+      super(title);         
       String[] x = {"1/3/2002","1/4/2002"};
       double[] y = {1313.13, 1232.24};
       final XYDataset dataset = createDataset(x, y);         
       final JFreeChart chart = createChart( dataset );         
-      final ChartPanel chartPanel = new ChartPanel( chart );         
-      chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 370 ) );         
-      chartPanel.setMouseZoomable( true , false );         
-      setContentPane( chartPanel );
+      final ChartPanel chartPanel = new ChartPanel(chart);         
+      chartPanel.setPreferredSize(new java.awt.Dimension(560, 370));         
+      chartPanel.setMouseZoomable(true, false);         
+      setContentPane(chartPanel);
    }
 
    private XYDataset createDataset(String[] x, double[] y) {
-      final TimePeriodValues series = new TimePeriodValues( "Data" );         
+      final TimePeriodValues series = new TimePeriodValues("Data");         
       for(int i = 0; i < x.length; i++) 
       {
           String[] parts = x[i].split("/");
@@ -38,7 +37,7 @@ public class TimeSeries_AWT extends ApplicationFrame {
       return TimePeriodValuesCollection(series);
    }     
 
-   private JFreeChart createChart( final XYDataset dataset ) {
+   private JFreeChart createChart(final XYDataset dataset) {
       return ChartFactory.createTimeSeriesChart(             
          "Stock Market Analysis", 
          "Date",              
@@ -49,11 +48,11 @@ public class TimeSeries_AWT extends ApplicationFrame {
          false);
    }
 
-   public static void main( final String[ ] args ) {
+   public static void main(final String[ ] args) {
       final String title = "Stock Market Analysis";         
-      final TimeSeries_AWT demo = new TimeSeries_AWT( title );         
-      demo.pack( );         
-      RefineryUtilities.positionFrameRandomly( demo );         
-      demo.setVisible( true );
+      final Graph demo = new Graph(title);         
+      demo.pack();         
+      RefineryUtilities.positionFrameRandomly(demo);         
+      demo.setVisible(true);
    }
 }   
