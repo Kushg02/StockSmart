328dac2df4b87c6795a6e79738893459fa7b0244
zhixinlucy <50589128+zhixinlucy@users.noreply.github.com>
2019-05-21 17:57:10 -0700

    Update TimeSeries_AWT.java

--- TimeSeries_AWT.java
+++ TimeSeries_AWT.java
@@ -16,9 +16,11 @@ public class TimeSeries_AWT extends ApplicationFrame {
       
       String[] x = {"1/3/2002","1/4/2002", "1/5/2002", "1/6/2002", "1/7/2002"};
       double[] y = {1313.13, 1232.24, 1313.13, 1353.13, 1213.13};
+      double[] z = {1132.32, 1242.43, 1253.53, 1353.32, 1125.14};
+      double[] w = {1511.54, 1534.53, 1564.35, 1645.34, 1542.24};
       
-      final XYDataset dataset = createDataset(x, y);         
-      final JFreeChart chart = createChart( dataset );  
+      final XYDataset dataset = createDataset(x, y, z, w);         
+      final JFreeChart chart = createChart(dataset);  
       
       final ChartPanel chartPanel = new ChartPanel(chart);         
       chartPanel.setPreferredSize(new java.awt.Dimension(560, 370));         
@@ -27,8 +29,8 @@ public class TimeSeries_AWT extends ApplicationFrame {
       setContentPane(chartPanel);
    }
 
-   private XYDataset createDataset(String[] x, double[] y) {
-      final TimePeriodValues s1 = new TimePeriodValues("Data");         
+   private XYDataset createDataset(String[] x, double[] y, double[] z, double[] w) {
+      final TimePeriodValues s1 = new TimePeriodValues("Data1");         
       for(int i = 0; i < x.length; i++) 
       {
           String[] parts = x[i].split("/");
@@ -39,8 +41,32 @@ public class TimeSeries_AWT extends ApplicationFrame {
           s1.add(new SimpleTimePeriod(date.getStart(), date.getEnd()), y[i]);
       }
       
+      final TimePeriodValues s2 = new TimePeriodValues("Data2");         
+      for(int i = 0; i < x.length; i++) 
+      {
+          String[] parts = x[i].split("/");
+          int day = Integer.parseInt(parts[1]);
+          int month = Integer.parseInt(parts[0]);
+          int year = Integer.parseInt(parts[2]);
+          Day date = new Day(day, month, year); //integer
+          s2.add(new SimpleTimePeriod(date.getStart(), date.getEnd()), z[i]);
+      }
+      
+      final TimePeriodValues s3 = new TimePeriodValues("Data3");         
+      for(int i = 0; i < x.length; i++) 
+      {
+          String[] parts = x[i].split("/");
+          int day = Integer.parseInt(parts[1]);
+          int month = Integer.parseInt(parts[0]);
+          int year = Integer.parseInt(parts[2]);
+          Day date = new Day(day, month, year); //integer
+          s3.add(new SimpleTimePeriod(date.getStart(), date.getEnd()), w[i]);
+      }
+      
       final TimePeriodValuesCollection dataset = new TimePeriodValuesCollection(); //Forked from http://www.java2s.com/Code/Java/Chart/JFreeChartTimePeriodValuesDemo2.htm
       dataset.addSeries(s1); //also forked
+      dataset.addSeries(s2);
+      dataset.addSeries(s3);
       dataset.setDomainIsPointsInTime(false); //also forked
       
       return dataset;
